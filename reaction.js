d1a32b798f12ddd0793827072393d0b7d97d9bb7
kaimcfarlane <kaimcfarlane43@gmail.com>
2022-06-27 12:22:22 -0400

    Added Drop Down for Games
    Added drop down list for games.

--- reaction.js
+++ reaction.js
@@ -2,3 +2,17 @@
 //Random dots, green means click, red means lost points/time
 //Song Lyrics, Random words, Book, WPM game
 //Click Timer (either with music beat or just as fast as possible, can be space bar or mouse clickers)
+
+listOpen = false;
+function openList() {
+    var list = document.getElementById("gameSelect");
+    if(listOpen) {
+        list.style.display = "none";
+        listOpen = false;
+    }
+    else {
+        
+        list.style.display = "block";
+        listOpen = true;
+    }
+}
