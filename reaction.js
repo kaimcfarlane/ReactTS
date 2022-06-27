1546cf815149370d89ccddc752289fb4b2a7e4df
kaimcfarlane <kaimcfarlane43@gmail.com>
2022-06-27 11:47:28 -0400

    Added Base HTMl and CSS for Home Page
    Added a animated CSS animation as well as game picker for index.html

---
+++ reaction.js
@@ -0,0 +1,4 @@
+//What I want
+//Random dots, green means click, red means lost points/time
+//Song Lyrics, Random words, Book, WPM game
+//Click Timer (either with music beat or just as fast as possible, can be space bar or mouse clickers)
