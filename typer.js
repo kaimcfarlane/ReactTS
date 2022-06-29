022e3d47e03d378b66d77ed26df2935c1b00cc81
kaimcfarlane <kaimcfarlane43@gmail.com>
2022-06-29 11:27:53 -0400

    Added Info Menu to all other game pages
    Addes the css, js, and html to all other game pages.

---
+++ typer.js
@@ -0,0 +1,22 @@
+var timerVis = false
+var timer = document.getElementById("time");
+let startTime = 30;
+function startGame() {
+    if(timerVis) {
+        timer.style.display = "none";
+        timerVis = false;
+    }
+    else {
+        timer.style.display = "block";
+        timerVis = true;
+        var itv1 = setInterval(function countDown() {
+            startTime--;
+            console.log("success " + startTime);
+            timer.innerHTML = "TIME: " + startTime;
+            if(startTime <= 0)
+            {
+                clearInterval(itv1);
+            }
+        }, 1000);
+    }
+}
