b023ee1d4f2ccfb8a104accd5184b3e6d4646806
kaimcfarlane <kaimcfarlane43@gmail.com>
2022-07-11 18:27:52 -0400

    Fixed span spacing bug
    Spans after periods took no space so make those spans take five pixels of width.

--- typer.js
+++ typer.js
@@ -20,6 +20,38 @@ function startGame() {
         timer.style.display = "none";
         timerVis = false;
     }
+    if(startTime<=0){
+        var helpText = document.getElementById("helpText");
+        if(helpText.style.display == "block") {
+            helpText.style.display = "none";
+        }
+        i=0;
+        score = 0;
+        textDisplay.innerText = "";
+        inputText.style.display = "inline-block";
+        LPM.innerText = "LPM";
+        WPM.innerText = "WPM";
+        startButton.innerHTML = "START";
+        startTime = 60;
+        timer.style.display = "block";
+        timerVis = true;
+        text = getRandomText();
+        returnSnippet(text);
+        var itv1 = setInterval(function countDown() {
+            startTime--;
+            console.log("success " + startTime);
+            timer.innerHTML = "TIME: " + startTime;
+            if(startTime <= 0 || startTime < 1)
+            {
+                clearInterval(itv1);
+                textDisplay.innerText = "TIMES UP!";
+                inputText.style.display = "none";
+                LPM.innerText = "LPM " + charTyped;
+                WPM.innerText = "WPM " + wordsTyped;
+                startButton.innerHTML = "PLAY AGAIN";
+            }
+        }, 1000);
+    }
     else {
         timer.style.display = "block";
         timerVis = true;
@@ -42,6 +74,7 @@ function startGame() {
                 inputText.style.display = "none";
                 LPM.innerText = "LPM " + charTyped;
                 WPM.innerText = "WPM " + wordsTyped;
+                startButton.innerHTML = "PLAY AGAIN";
             }
         }, 1000);
     }
@@ -119,9 +152,20 @@ function returnSnippet(text) {
     i+=20;
     textDisplay.innerText = null;
     inputText.value = null;
+    var periodFound = false;
     newText.split('').forEach(character => {
         const newSpan = document.createElement('span');
         newSpan.innerText = character;
+        if(periodFound)
+        {
+            periodFound = false;
+            newSpan.style.display = "inline-block";
+            newSpan.style.width = "5px";
+        }
+        if(character == ".")
+        {
+            periodFound = true;
+        }
         textDisplay.appendChild(newSpan);
     })
 }
@@ -248,198 +292,198 @@ function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-const easySongs = ["I've never seen a diamond in the flesh \
-I cut my teeth on wedding rings in the movies \
-And I'm not proud of my address \
-In a torn up town, no postcode envy \
-But every song's like \
-Gold teeth, Grey Goose, trippin' in the bathroom \
-Bloodstains, ball gowns, trashin' the hotel room \
-We don't care \
-We're driving Cadillacs in our dreams \
-But everybody's like \
-Cristal, Maybach, diamonds on your timepiece \
-Jet planes, islands, tigers on a gold leash \
-We don't care \
-We aren't caught up in your love affair \
-And we'll never be royals (royals) \
-It don't run in our blood \
-That kind of luxe just ain't for us \
-We crave a different kind of buzz \
-Let me be your ruler (ruler) \
-You can call me queen bee \
-And baby, I'll rule (I'll rule, I'll rule, I'll rule) \
-Let me live that fantasy",
+const easySongs = ["I've never seen a diamond in the flesh. \
+I cut my teeth on wedding rings in the movies. \
+And I'm not proud of my address. \
+In a torn up town, no postcode envy. \
+But every song's like. \
+Gold teeth, Grey Goose, trippin' in the bathroom. \
+Bloodstains, ball gowns, trashin' the hotel room. \
+We don't care. \
+We're driving Cadillacs in our dreams .\
+But everybody's like. \
+Cristal, Maybach, diamonds on your timepiece. \
+Jet planes, islands, tigers on a gold leash. \
+We don't care. \
+We aren't caught up in your love affair. \
+And we'll never be royals (royals). \
+It don't run in our blood. \
+That kind of luxe just ain't for us. \
+We crave a different kind of buzz. \
+Let me be your ruler (ruler). \
+You can call me queen bee. \
+And baby, I'll rule (I'll rule, I'll rule, I'll rule). \
+Let me live that fantasy.",
 
-"Shine bright like a diamond \
-Shine bright like a diamond \
-Find light in the beautiful sea \
-I choose to be happy \
-You and I, you and I \
-We're like diamonds in the sky \
-You're a shooting star I see \
-A vision of ecstasy \
-When you hold me, I'm alive \
-We're like diamonds in the sky \
-I knew that we'd become one right away \
-Oh, right away \
-At first sight I felt the energy of sun rays \
-I saw the life inside your eyes \
-So shine bright, tonight, you and I \
-We're beautiful like diamonds in the sky \
-Eye to eye, so alive \
-We're beautiful like diamonds in the sky",
+"Shine bright like a diamond. \
+Shine bright like a diamond. \
+Find light in the beautiful sea. \
+I choose to be happy. \
+You and I, you and I. \
+We're like diamonds in the sky. \
+You're a shooting star I see. \
+A vision of ecstasy. \
+When you hold me, I'm alive. \
+We're like diamonds in the sky. \
+I knew that we'd become one right away. \
+Oh, right away. \
+At first sight I felt the energy of sun rays. \
+I saw the life inside your eyes. \
+So shine bright, tonight, you and I. \
+We're beautiful like diamonds in the sky. \
+Eye to eye, so alive. \
+We're beautiful like diamonds in the sky.",
 
-"Right, my yiy just changed \
-You just buzzed the front gate \
-I thank God you came \
-How many more days could I wait? \
-I made plans with you \
-And I won't let em fall through \
-I, I, I, I, I \
-I think I'd lie for you \
-I think I'd die for you \
-Jodeci Cry For You \
-Do things when you want me to \
-Like controlla, controlla \
-Yeah, like controlla, controlla \
-Yeah, okay, you like it \
-When I get, aggressive, tell you to \
-Go slower, go faster \
-Like controlla, controlla \
-Yeah, like controlla, controlla"
+"Right, my yiy just changed. \
+You just buzzed the front gate. \
+I thank God you came. \
+How many more days could I wait?. \
+I made plans with you. \
+And I won't let em fall through. \
+I, I, I, I, I. \
+I think I'd lie for you. \
+I think I'd die for you. \
+Jodeci Cry For You. \
+Do things when you want me to. \
+Like controlla, controlla. \
+Yeah, like controlla, controlla. \
+Yeah, okay, you like it. \
+When I get, aggressive, tell you to. \
+Go slower, go faster. \
+Like controlla, controlla. \
+Yeah, like controlla, controlla."
 
 ];
 
-const medSongs = ["Sometimes, all I think about is you \
-Late nights in the middle of June \
-Heat waves been fakin’ me out \
-Can’t make you happier now \
-Sometimes, all I think about is you \
-Late nights in the middle of June \
-Heat waves been fakin’ me out \
-Can’t make you happier now \
-Usually I put somethin’ on TV \
-So we never think about you and me \
-But today I see our reflections clearly \
-In Hollywood, layin’ on the screen \
-You just need a better life than this \
-You need somethin’ I can never give \
-Fake water all across the road \
-It’s gone now, the night has come, but \
-Sometimes all I think about is you \
-Late nights in the middle of June \
-Heat waves been fakin’ me out \
-Can’t make you happier now",
+const medSongs = ["Sometimes, all I think about is you. \
+Late nights in the middle of June. \
+Heat waves been fakin’ me out. \
+Can’t make you happier now. \
+Sometimes, all I think about is you. \
+Late nights in the middle of June. \
+Heat waves been fakin’ me out. \
+Can’t make you happier now. \
+Usually I put somethin’ on TV. \
+So we never think about you and me. \
+But today I see our reflections clearly. \
+In Hollywood, layin’ on the screen. \
+You just need a better life than this. \
+You need somethin’ I can never give. \
+Fake water all across the road. \
+It’s gone now, the night has come, but. \
+Sometimes all I think about is you. \
+Late nights in the middle of June. \
+Heat waves been fakin’ me out. \
+Can’t make you happier now.",
 
-"Turn up the radio \
-Blast your stereo \
-Right now \
-This joint is fizzlin \
-It's sizzlin \
-Right \
-Yo check this out right here \
-Dude wanna hate on us (dude) \
-Dude need to ease on up (dude) \
-Dude wanna act on up \
-But dude get shut like flava shut (down) \
-Chick say she ain't down \
-But chick backstage when we in town (ha) \
-She like man on drum (boom) \
-She wanna hit n' run (err) \
-Yeah, that's the speed \
-That's who we do \
-That's who we be \
-B-L-A-C-K E-Y-E-D P to the E \
-Then the A to the S \
-When we play you shake your ass \
-Shake it, shake it, shake it girl \
-Make sure you don't break it, girl \
-Cause we gonna",
+"Turn up the radio. \
+Blast your stereo. \
+Right now. \
+This joint is fizzlin. \
+It's sizzlin. \
+Right. \
+Yo check this out right here. \
+Dude wanna hate on us (dude). \
+Dude need to ease on up (dude). \
+Dude wanna act on up. \
+But dude get shut like flava shut (down). \
+Chick say she ain't down. \
+But chick backstage when we in town (ha). \
+She like man on drum (boom). \
+She wanna hit n' run (err). \
+Yeah, that's the speed. \
+That's who we do. \
+That's who we be. \
+B-L-A-C-K E-Y-E-D P to the E. \
+Then the A to the S. \
+When we play you shake your ass. \
+Shake it, shake it, shake it girl. \
+Make sure you don't break it, girl. \
+Cause we gonna.",
 
-"I love the way you walk into the room \
-Body shining lightin' up the place \
-And when you talk, everybody stop \
-'Cause they know you know just what you sayin \
-The way that you protect your friends \
-Baby I respect you for that \
-And when you grow you'll take everyone you love along \
-I love that stuff \
-Don't fly me away \
-Don't need to buy a diamond key to unlock my heart \
-You shelter my soul, you're my fire when I'm cold \
-I want you to know \
-You had me at hello \
-Hello, hello \
-'Cause you had me at hello \
-Hello, hello \
-'Cause it was many years ago \
-Baby when you stole my cool \
-'Cause you had me at hello \
-Hello, hello \
-I get so excited when you travel with me \
-Baby while I'm on my grind \
-And never will I ever let my hustle \
-Come between me and my family time and \
-You keep me humble, I like this type \
-'Cause you know there's more to life \
-And if I need ya, you will be here \
-You will make the sacrifice"
+"I love the way you walk into the room. \
+Body shining lightin' up the place. \
+And when you talk, everybody stop. \
+'Cause they know you know just what you sayin. \
+The way that you protect your friends. \
+Baby I respect you for that. \
+And when you grow you'll take everyone you love along. \
+I love that stuff. \
+Don't fly me away. \
+Don't need to buy a diamond key to unlock my heart. \
+You shelter my soul, you're my fire when I'm cold. \
+I want you to know. \
+You had me at hello. \
+Hello, hello. \
+'Cause you had me at hello. \
+Hello, hello. \
+'Cause it was many years ago. \
+Baby when you stole my cool. \
+'Cause you had me at hello. \
+Hello, hello. \
+I get so excited when you travel with me. \
+Baby while I'm on my grind. \
+And never will I ever let my hustle. \
+Come between me and my family time and. \
+You keep me humble, I like this type. \
+'Cause you know there's more to life. \
+And if I need ya, you will be here. \
+You will make the sacrifice."
 ];
 
-const hardSongs = ["Sí, sabes que ya llevo un rato mirándote \
-Tengo que bailar contigo hoy \
-Vi que tu mirada ya estaba llamándome \
-Muéstrame el camino que yo voy \
-Oh, tú, tú eres el imán y yo soy el metal \
+const hardSongs = ["Sí, sabes que ya llevo un rato mirándote. \
+Tengo que bailar contigo hoy. \
+Vi que tu mirada ya estaba llamándome. \
+Muéstrame el camino que yo voy. \
+Oh, tú, tú eres el imán y yo soy el metal. \
 Me voy acercando y voy armando el plan \
-Solo con pensarlo se acelera el pulso \
+Solo con pensarlo se acelera el pulso. \
 Ya, ya me estás gustando más de lo normal \
-Todos mis sentidos van pidiendo más \
+Todos mis sentidos van pidiendo más. \
 Esto hay que tomarlo sin ningún apuro \
-Despacito \
-Quiero respirar tu cuello despacito \
-Deja que te diga cosas al oído \
-Para que te acuerdes si no estás conmigo \
-Despacito", 
+Despacito. \
+Quiero respirar tu cuello despacito. \
+Deja que te diga cosas al oído. \
+Para que te acuerdes si no estás conmigo. \
+Despacito.", 
 
-"This flippity dippity-hippity hip-hop \
-You don't really wanna get into a pushin' match \
-With this rappity brat, packin' a MAC in the back of the Ac' \
-Backpack rap crap, yap-yap, yackety-yack \
-And at the exact same time, I attempt these lyrical acrobat stunts while I'm practicin' that \
-I'll still be able to break a fricken' table \
-Over the back of a couple of maggots and crack it in half \
-Only realized it was ironic, I was signed to Aftermath after the fact \
-How could I not blow? All I do is drop bombs \
+"This flippity dippity-hippity hip-hop. \
+You don't really wanna get into a pushin' match. \
+With this rappity brat, packin' a MAC in the back of the Ac'. \
+Backpack rap crap, yap-yap, yackety-yack. \
+And at the exact same time, I attempt these lyrical acrobat stunts while I'm practicin' that. \
+I'll still be able to break a fricken' table. \
+Over the back of a couple of maggots and crack it in half. \
+Only realized it was ironic, I was signed to Aftermath after the fact. \
+How could I not blow? All I do is drop bombs. \
 Feel my wrath of attack \
-Rappers are havin' a rough time period, here's a maxi pad \
-It's actually disastrously bad for the wack \
-While I'm masterfully constructing this masterpièce \
-'Cause I'm beginnin' to feel like a Rap God, Rap God \
-All my people from the front to the back nod, back nod \
+Rappers are havin' a rough time period, here's a maxi pad. \
+It's actually disastrously bad for the wack. \
+While I'm masterfully constructing this masterpièce. \
+'Cause I'm beginnin' to feel like a Rap God, Rap God. \
+All my people from the front to the back nod, back nod. \
 Now, who thinks their arms are long enough to slap box, slap box? \
-Let me show you maintainin' this shit ain't that hard, that hard \
-Everybody want the key and the secret to rap immortality like Ι have got",
+Let me show you maintainin' this shit ain't that hard, that hard. \
+Everybody want the key and the secret to rap immortality like Ι have got.",
 
-"Babylon position the queen and set the pawn \
-And start transform like Deceptacon \
-Anytime delegates have a discrepence \
-Well a bare tension with some long weapons \
-Night vision upon the attack mission \
-Colaliton of folly-ticians get switch on \
-If you look in the face of the newly born \
-The newly born face malnutrition \
-Suspicion to what is them ambition \
-Total destruction start to ignition \
-And the world still a fight over religion \
-Everyone have a right to a decision \
-Superstition the people reflect upon, something nuh right \
-I man suspect a con \
-Cause them lost inna darkness beyond \
-And none shall escape except the ones, who \
+"Babylon position the queen and set the pawn.   \
+And start transform like Deceptacon. \
+Anytime delegates have a discrepence. \
+Well a bare tension with some long weapons. \
+Night vision upon the attack mission. \
+Colaliton of folly-ticians get switch on. \
+If you look in the face of the newly born. \
+The newly born face malnutrition. \
+Suspicion to what is them ambition. \
+Total destruction start to ignition. \
+And the world still a fight over religion. \
+Everyone have a right to a decision. \
+Superstition the people reflect upon, something nuh right. \
+I man suspect a con. \
+Cause them lost inna darkness beyond. \
+And none shall escape except the ones, who. \
 Exodus with no question, \
-Better put on your khaki uniform"
+Better put on your khaki uniform."
 ]
 
 const easyWords = [""];
